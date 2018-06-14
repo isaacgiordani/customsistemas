9d3374b5f0b111ef633ac92cd6e88585fb3e1595
Isaac Giordani <isaacgiordani@gmail.com>
2018-06-14 12:06:51 -0300

    filtro

--- listatelefonica/admin.py
+++ listatelefonica/admin.py
@@ -6,18 +6,22 @@ from .models import Entity, PhoneBook, AccessType, Access
 class EntityAdmin(admin.ModelAdmin):
     list_display = ['name','slug','created','modified']
     search_fields = ['name','slug']
+    list_filter = ['created', 'modified']
 
 class PhoneBookAdmin(admin.ModelAdmin):
     list_display = ['name','lastname','slug','entity','modified']
     search_fields = ['name','slug']
+    list_filter = ['created', 'modified']
 
 class AccessTypeAdmin(admin.ModelAdmin):
     list_display = ['description','created','modified']
     search_fields = ['description']
+    list_filter = ['created', 'modified']
 
 class AccessAdmin(admin.ModelAdmin):
     list_display = ['entity','accesstype','identifier','password','description','modified']
     search_fields = ['entity','accesstype','identifier']
+    list_filter = ['created', 'modified']
 
 admin.site.register(Entity, EntityAdmin)
 admin.site.register(PhoneBook, PhoneBookAdmin)
